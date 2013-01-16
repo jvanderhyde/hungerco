eb2cc1014ef588f797e5886be339792fcb06de36
jamesvanderhyde <jamesvanderhyde@win-7rqudand37e.benedictine.edu>
2013-01-16 13:38:22 -0600

    Formatted code.

--- Hungerco/src/hungerco/Hungerco.java
+++ Hungerco/src/hungerco/Hungerco.java
@@ -1,19 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+//Main program
+//Created by Daisuke Mori, 16 January 2013
+//Modified by James Vanderhyde, 16 January 2013
+//  Formatted code and added comments.
+
 package hungerco;
 
 /**
  *
  * @author dai
  */
-public class Hungerco {
+public class Hungerco
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args)
+    {
+
     }
 }
