240b6cc5b2b0835c4c32c6bf1ecec75234a0849e
Daisuke <Daisuke@localhost>
2013-01-22 23:56:39 -0600

    Just make to get destination .php

---
+++ HCApp/createAccount.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Create an Account</title>
+    </head>
+    <body>
+        <h1>Create an Account</h1>
+        <?php
+        echo "test create an account<br />";
+        ?>
+        
+        
+    </body>
+</html>
