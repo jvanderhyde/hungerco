a86f51274329b70cdcf97a8ad6cf869345507b62
Daisuke <Daisuke@localhost>
2013-01-30 01:21:56 -0600

    put the forms to make an account

--- HCApp/createAccount.php
+++ HCApp/createAccount.php
@@ -6,9 +6,23 @@
     </head>
     <body>
         <h1>Create an Account</h1>
-        <?php
-        echo "test create an account<br />";
-        ?>
+        <form name="createaccount" action="submitcreation.php" method="POST">
+            <h4>Name</h4>
+            <input type="text" size="40" name="name">
+            <h4>Student ID</h4>
+            <input type="text" size="40" name="id">
+            <h4>Phone</h4>
+            <input type="text" size="40" name="phone">
+            <h4>Password</h4>
+            <input type="password" size="40" name="password1">
+            <h4>Re-enter Password</h4>
+            <input type="password" size="40" name="password2">
+            <br/><br/>
+            <input type="submit" value="Create">
+        </form>
+        <form name="cancel" action="stlogin.php" method="POST">
+            <input type="submit" value="Cancel">
+        </form>
         
         
     </body>
