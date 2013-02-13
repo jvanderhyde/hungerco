49acc1e9968e5f67d7e319cd320c52c064b51288
Amanda <Amanda@localhost>
2013-02-13 10:21:39 -0600

    Cleaned up session verification and added logout

--- HCApp/verify_staff_login.php
+++ HCApp/verify_staff_login.php
@@ -1,13 +1,13 @@
 <?php
 
 $host="localhost"; // Host name
-$username="hungerco"; // Mysql username
-$password="intensiveness"; // Mysql password
+$dbusername="hungerco"; // Mysql username
+$dbpassword="intensiveness"; // Mysql password
 $db_name="hungerco"; // Database name
 $tbl_name="staff"; // Table name
 
 // Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
+mysql_connect("$host", "$dbusername", "$dbpassword")or die("cannot connect");
 mysql_select_db("$db_name")or die("cannot select DB");
 
 // username and password sent from form
@@ -15,11 +15,11 @@ $username=$_POST['staffName'];
 $password=$_POST['staffPass'];
 
 // To protect MySQL injection
-$username = stripslashes($username);
-$password = stripslashes($password);
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
-$sql="SELECT * FROM $tbl_name WHERE Staffname='$username' and Staffpass='$password'";
+$stripusername = stripslashes($username);
+$strippassword = stripslashes($password);
+$cleanusername = mysql_real_escape_string($stripusername);
+$cleanpassword = mysql_real_escape_string($strippassword);
+$sql="SELECT * FROM $tbl_name WHERE Staffname='$cleanusername' and Staffpass='$cleanpassword'";
 $result=mysql_query($sql);
 
 // Mysql_num_row is counting table row
@@ -29,8 +29,8 @@ $count=mysql_num_rows($result);
 if($count==1){
 
 // Register $username, $password and redirect to file "login_success.php"
-session_register("username");
-session_register("password");
+session_register($cleanusername);
+session_register($cleanpassword);
 if(mysql_result($result,0,"type")=="Cafe")
     header("location:cafeinfo.php");
 }
