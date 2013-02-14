b803a1418555592a85089cd53ce3ba9a8c4bd813
Daisuke <Daisuke@localhost>
2013-02-14 17:00:18 -0600

    make class and function for stinfo and put the logout button

--- HCApp/stlogin.php
+++ HCApp/stlogin.php
@@ -1,4 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<?php 
+session_start();
+session_destroy();
+?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
--- HCApp/submitcreation.php
+++ HCApp/submitcreation.php
@@ -1,4 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<?php
+if(!isset($_POST['id'])){
+     header("location:stlogin.php");
+ }
+?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
