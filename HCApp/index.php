ac877dd139293ed2397d798415db3dca4c3c7385
Daisuke <Daisuke@localhost>
2013-04-06 17:49:48 -0500

    modify staccount.php and some .phps

--- HCApp/index.php
+++ HCApp/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Hunger Coalition</title>
     </head>
     <body>
         <form name="stlogin" action="stlogin.php" method="POST">
