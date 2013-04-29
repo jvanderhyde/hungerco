13db4a2dfcfed50508d6abec770724451235aa4c
Amanda <Amanda@localhost>
2013-04-29 09:31:03 -0500

    Formated Officer Volunteer and put Volunteer List with Vol Opp Info

--- HCApp/volunteers.php
+++ HCApp/volunteers.php
@@ -1,55 +1,59 @@
+<!DOCTYPE HTML>
+<?php
+    include_once 'functions.php';
+    include_once 'dbfunctions.php';
+    verifyuser(array("Officer"));
+
+    $vol_opps = getVolOpps();   
+    if(isset($_POST['volOpp']))
+        $volnum = $_POST['volOpp'];
+    else if(isset($vol_opps[0]))
+        $volnum = $vol_opps[0]['oppnum'];
+
+    $volunteers = setVols($volnum);
+?>
 <html>
     <head> 
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Volunteers</title>
+        <meta name="author" content="BCCS">
+        <link rel="stylesheet" type="text/css" href="reset.css">
         <link rel="stylesheet" type="text/css" href="hcstylesheet.css">
     </head> 
     <body> 
-        <?php
-            include_once 'functions.php';
-            include_once 'dbfunctions.php';
-            verifyuser(array("Officer"));
+        <div id="page-container">
+            <?php officermenu(); ?>        
+            <div id="content">        
+                <h1>Volunteers</h1>
+                <form name="selectOpp" action="volunteers.php" method="POST">
+                    <select name='volOpp'>
+                        <?php 
+                            foreach ($vol_opps as $opp) 
+                            {
+                                echo "<option value='".$opp['oppnum']."'>".
+                                        $opp['oppname']." ".$opp['date']."</option>";
+                            }
+                        ?>
+                    </select>
+                    <input type="submit" name="button" value="View Volunteers">
+                </form>
 
-            $vol_opps = getVolOpps();   
-            if(isset($_POST['volOpp']))
-                $volnum = $_POST['volOpp'];
-            else if(isset($vol_opps[0]))
-                $volnum = $vol_opps[0]['oppnum'];
-            
-            $volunteers = setVols($volnum);
-        ?>
-        
-        <h1>Volunteers</h1>
-        <form name="selectOpp" action="volunteers.php" method="POST">
-            <select name='volOpp'>
+                <table border="1">
+                <tr>
+                <th>Name</th>
+                </tr>
                 <?php 
-                    foreach ($vol_opps as $opp) 
+                    foreach ($volunteers as $student) 
                     {
-                        echo "<option value='".$opp['oppnum']."'>".
-                                $opp['oppname']." ".$opp['date']."</option>";
+                        echo "<tr>";
+                        echo "<td>".$student['fname']." ".$student['lname']."</td>";
+                        echo "</tr>";
                     }
                 ?>
-            </select>
-            <input type="submit" name="button" value="View Volunteers">
-        </form>
-        
-        <table border="1">
-        <tr>
-        <th>Name</th>
-        </tr>
-        <?php 
-            foreach ($volunteers as $student) 
-            {
-                echo "<tr>";
-                echo "<td>".$student['fname']." ".$student['lname']."</td>";
-                echo "</tr>";
-            }
-        ?>
-        </table>
-
-        <form name="home" action="officerhome.php" method="POST">
-            <input type="submit" value="Back to Home Page">
-        </form>
-
+                </table>
+            </div>
+            <?php footer();  ?>
+        </div>
     </body>
 </html>
 
