3b8ed6f9cae372fbdf29e871bf0dc5a02a742971
jamesvanderhyde <jamesvanderhyde@James-Vanderhydes-MacBook-Pro.local>
2013-07-24 13:08:08 -0500

    Made it easier to switch between database servers

--- HCApp/dbfunctions.php
+++ HCApp/dbfunctions.php
@@ -3,16 +3,7 @@
 
 include_once "functions.php";
 
-function connectToDB()
-{
-    $username="hungerco";
-    $password="intensiveness";
-    $database="hungerco";
-    $host="ec2-72-44-43-163.compute-1.amazonaws.com";                           
-    $link = mysql_connect($host,$username,$password);
-    @mysql_select_db($database, $link) or die( "Unable to select database");
-    return $link;
-}
+require('dbconnect_localhost.php');
 
 //Student
 
