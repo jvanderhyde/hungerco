c1a606809db59f74a66631d02afeebeba5392952
Amanda <Amanda@localhost>
2013-09-02 15:57:28 -0500

    Put username back for actual website

--- HCApp/dbconnect_localhost.php
+++ HCApp/dbconnect_localhost.php
@@ -2,11 +2,11 @@
 
 function connectToDB()
 {
-    $username="root";//"hungerco";
+    $username="hungerco";
     $password="intensiveness";
     $database="hungerco";
     $host="localhost";                           
-    $link = mysql_connect($host,$username);
+    $link = mysql_connect($host,$username,$password);
     @mysql_select_db($database, $link) or die( "Unable to select database");
     return $link;
 }
